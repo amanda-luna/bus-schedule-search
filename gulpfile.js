ae8e4e497dca6b664cb0c429ea54af7180455a49
Amanda Luna <amandab.luna@gmail.com>
2020-12-17 11:48:32 -0600

    Creating gulpfile and reorganizing js and css folders

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, series} = require('gulp');
+const concat = require('gulp-concat');
+const minifyjs = require('gulp-minify');
+const cssnano = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const optimizeimg = require('gulp-imagemin');
+
+function htmlTask() {
+  return src('src/*.html')
+    .pipe(dest('dist'))
+}
+
+function jsTask() {
+  return src('src/scripts/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.js'))
+    .pipe(minifyjs())
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist/js'))
+}
+
+function cssTask() {
+  return src('src/styles/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.css'))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist/css'))
+}
+
+exports.default = series(htmlTask, jsTask, cssTask);
